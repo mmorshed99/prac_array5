5dbe4871934b8428d011e62fe8196c47bcf518e2
M Manzur Morshed <mmorshed@asu.edu>
2017-08-09 20:28:46 -0400

    Update merge_overlapping_interval.py

--- merge_overlapping_interval.py
+++ merge_overlapping_interval.py
@@ -1,3 +1,13 @@
+#Given a collection of intervals, merge all overlapping intervals.
+#
+#For example:
+#
+#Given [1,3],[2,6],[8,10],[15,18],
+#
+#return [1,6],[8,10],[15,18].
+#
+#Make sure the returned intervals are sorted.
+#
 # Definition for an interval.
 # class Interval:
 #     def __init__(self, s=0, e=0):
