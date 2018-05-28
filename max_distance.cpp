303a096609e60b9c39bf202a9dfca481248b6d7d
M Manzur Morshed <mmorshed@asu.edu>
2018-05-27 22:54:44 -0400

    Create max_distance.cpp

---
+++ max_distance.cpp
@@ -0,0 +1,32 @@
+int Solution::maximumGap(const vector<int> &A) {
+    vector<bool> myArr(A.size());
+    int min = A[0];
+    int max_distance = 0;
+    int i = A.size()-1;
+    int j = A.size()-1;
+    
+    for(int i = 0; i < A.size(); i++){
+        if(A[i] > min){
+            myArr[i] = false;
+        }
+        else{
+            min = A[i];
+            myArr[i] = true;
+        }
+    }
+
+    while(i >= 0){
+        if(myArr[i] == false){
+            i--;
+            continue;
+        }
+        while((A[i] > A[j]) && (j > i)){
+            j--;
+        }
+        if((j-i) > max_distance){
+            max_distance = j-i;
+        }
+        i--;
+    }
+    return max_distance;
+}
